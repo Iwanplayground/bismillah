c5b364d59389461e6a16129c23ea3f1cb903e351
iwanplayground <backenddeviwan@gmail.com>
2022-04-29 06:05:50 +0700

    menambah param pd fungsi baru

--- overriding.py
+++ overriding.py
@@ -19,3 +19,18 @@ sepeda = kendaraan()
 vario = motor()
 
 sepeda.berjalan()
+print('==='*30)
+# kita dapaat menambahkan paramater pd fungsi yg diminta, contoh:
+class kendaraan:
+    def berjalan(self):
+        print('berjalan...')
+
+class motor(kendaraan):
+    def berjalan(self,kecepatan,satuan='km/jam'):
+        print(f'berjalan dengan kecepatan {kecepatan}{satuan}')
+
+sepeda = kendaraan()
+cbr = motor()
+
+sepeda.berjalan()
+cbr.berjalan(180)
