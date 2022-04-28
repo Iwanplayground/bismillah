f2b0fb93116a89c24c27e32164ee30c35986961d
iwanplayground <backenddeviwan@gmail.com>
2022-04-28 11:51:41 +0700

    menamatkan inherit sampe konstruktor

--- inheritance.py
+++ inheritance.py
@@ -24,3 +24,31 @@ somad = mhsorganisasi('somade','UGM')
 andi.perkenalan()
 somad.perkenalan()
 
+print('==='*30)
+
+# Membuat konstruktor pada kelas turunan
+'''
+Apa yang terjadi ketika kelas turunan memiliki konstruktor sendiri?
+Ia akan menimpa konstruktor dari kelas induk sehingga konstruktor 
+kelas induk tidak akan pernah dieksekusi.
+
+konstruktor kelas parent tidak lagi dieksekusi ketika kita membuat instance dari kelas child. 
+Hal itu terjadi karena sekarang kedua kelas tersebut telah memiliki konstruktor sendiri
+
+'''
+
+# jelasnya kita tidak ingin menimpa konstruk pada kelas parent, maka solusi yang dapat dilakukan adalah:
+'''
+yang pertama menggunakan fungsi super().__init__()
+dan yang kedua menggunakan classparent.__init__()
+'''
+
+class mhsjurusan(mhs):
+    def __init__(self, nama, kampus):
+        super().__init__(nama, kampus)
+
+
+andi = mhsjurusan('andi','UI')
+andi.perkenalan()
+
+# untuk nambah properti/item baru yg ndada di class parent ya tinggal tambah pake self aja di childnya.
